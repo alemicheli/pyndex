3326eb03049f95ad506c969c4d78eb8d97d743a1
alemicheli <alessandromicheli01@gmail.com>
2020-06-10 19:56:15 +0100

    Added index file

---
+++ pyndex/core/index.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("../util")
+import _decorator as dec
+import algorithm as alg
+import wrds
+import pandas as pd
+
+class Index:
+
+    @staticmethod
+    @dec.typeassert(wrds = wrds.sql.Connection,
+                    year = int, index = str)
+    @dec.supported(index = ["1000","2000","3000"])
+    def from_wrds(wrds, year, index, verbose = False):
+        if verbose :
+            print("Downloading Data ...")
+        metadata, stock = alg.download(wrds,year)
+        if verbose :
+            print("Building Index ...")
+        weights_table = alg.build(stock, year, index,
+                        metadata)
+
+        return weights_table
+
+    @staticmethod
+    @dec.typeassert(year = int)
+    def get_calendar(year):
+        calendar = alg.get_calendar(year)
+        calendar_table = pd.DataFrame(
+                        calendar.items(),columns=["event","date"])
+        return calendar_table
+                
