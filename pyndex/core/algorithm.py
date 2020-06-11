a4b249ee5ecaeeb90a8f82d7b776818e78bfce72
alemicheli <alessandromicheli01@gmail.com>
2020-06-11 17:19:00 +0100

    Pep8 applied

--- pyndex/core/algorithm.py
+++ pyndex/core/algorithm.py
@@ -1,80 +1,89 @@
+from pandas.tseries.offsets import BDay
+import pdb
+import pandas as pd
+import datetime as dt
+import calendar
+import pyndex.util._decorator as dec
 import sys
 sys.path.append("../util")
-import pyndex.util._decorator as dec
-import calendar
-import datetime as dt
-import pandas as pd
-import pdb
-from pandas.tseries.offsets import BDay
 
-#-------------------Calendar-----------------------
+# -------------------Calendar-----------------------
+
+
 @dec.validatebday(rank_day=True)
-def get_annual_rank(year):
-    return dt.date(year,5,31)
+def _get_annual_rank(year):
+    return dt.date(year, 5, 31)
 
-@dec.validatebday(rank_day = False)
-def get_rebalance(year,quarter):
-    month = { 2: 6, 3: 9, 4: 12, 1 : 3}
-    if quarter != 2 :
+
+@dec.validatebday(rank_day=False)
+def _get_rebalance(year, quarter):
+    month = {2: 6, 3: 9, 4: 12, 1: 3}
+    if quarter != 2:
         if quarter == 1:
-            year =  year+1
-        fridays = [week[calendar.FRIDAY] 
-                    for week in calendar.monthcalendar(year, month[quarter])]
+            year = year+1
+        fridays = [week[calendar.FRIDAY]
+                   for week in calendar.monthcalendar(year, month[quarter])]
         if fridays[0] == 0:
-            return dt.date(year,month[quarter],fridays[3])
+            return dt.date(year, month[quarter], fridays[3])
 
         else:
-            return dt.date(year,month[quarter],fridays[2])
+            return dt.date(year, month[quarter], fridays[2])
     else:
-        return dt.date(year,month[quarter], max([week[calendar.FRIDAY]
-            for week in calendar.monthcalendar(year, month[quarter])]))
+        return dt.date(year, month[quarter], max([week[calendar.FRIDAY]
+                                                  for week in calendar.monthcalendar(year, month[quarter])]))
 
-@dec.validatebday(rank_day = True)
-def get_quarter_rank(quarter_rebalance):
+
+@dec.validatebday(rank_day=True)
+def _get_quarter_rank(quarter_rebalance):
     preceeding_weeks = 5
-    return (quarter_rebalance - dt.timedelta(days = preceeding_weeks*7))
+    return (quarter_rebalance - dt.timedelta(days=preceeding_weeks*7))
 
-def get_calendar(year):
+
+def _get_calendar(year):
 
     cal = {}
-    cal["Annual Rank Day"] = get_annual_rank(year)
-    cal["Annual Rebalance Day"] = get_rebalance(year,2)
-    cal["Q3 Rebalance Day"] = get_rebalance(year,3)
-    cal["Q4 Rebalance Day"] = get_rebalance(year,4)
-    cal["Q1 Rebalance Day"] = get_rebalance(year,1)
-    cal["Q3 Rank Day"] = get_quarter_rank(cal["Q3 Rebalance Day"])
-    cal["Q4 Rank Day"] = get_quarter_rank(cal["Q4 Rebalance Day"])
-    cal["Q1 Rank Day"] = get_quarter_rank(cal["Q1 Rebalance Day"])
+    cal["Annual Rank Day"] = _get_annual_rank(year)
+    cal["Annual Rebalance Day"] = _get_rebalance(year, 2)
+    cal["Q3 Rebalance Day"] = _get_rebalance(year, 3)
+    cal["Q4 Rebalance Day"] = _get_rebalance(year, 4)
+    cal["Q1 Rebalance Day"] = _get_rebalance(year, 1)
+    cal["Q3 Rank Day"] = _get_quarter_rank(cal["Q3 Rebalance Day"])
+    cal["Q4 Rank Day"] = _get_quarter_rank(cal["Q4 Rebalance Day"])
+    cal["Q1 Rank Day"] = _get_quarter_rank(cal["Q1 Rebalance Day"])
 
     return cal
 
-def match_rank_w_rebalance(cal,year):
+
+def _match_rank_w_rebalance(cal, year):
     match = {}
     match[cal["Annual Rank Day"]] = cal["Annual Rebalance Day"]
-    if year >2003:
+    if year > 2003:
         match[cal["Q3 Rank Day"]] = cal["Q3 Rebalance Day"]
         match[cal["Q4 Rank Day"]] = cal["Q4 Rebalance Day"]
         match[cal["Q1 Rank Day"]] = cal["Q1 Rebalance Day"]
     return match
-#-------------------Download-----------------------
+# -------------------Download-----------------------
 
-def validate_year(wrds, year):
+
+def _validate_year(wrds, year):
     last_date = wrds.raw_sql('''SELECT date FROM crsp_a_stock.dsf 
     ORDER BY date DESC LIMIT 1''')["date"].get(0)
-    valid_year = (year >= 1989) & (get_quarter_rank(get_rebalance(year,1)) <= last_date)
+    valid_year = (year >= 1989) & (
+        _get_quarter_rank(_get_rebalance(year, 1)) <= last_date)
     return valid_year
 
-def download(wrds, year):
-    if not validate_year(wrds, year):
+
+def _download(wrds, year):
+    if not _validate_year(wrds, year):
         raise ValueError(f"Argument year with value {year} not supported")
-    
+
     stock_query = """
     SELECT date, permco, permno, cusip, prc, shrout, cfacpr FROM crsp_a_stock.dsf
     WHERE date BETWEEN '{rank_y}'
     AND '{rank_y_plus_1}'
     ORDER BY date
-    """.format(rank_y = get_annual_rank(year),
-        rank_y_plus_1 =  get_annual_rank(year+1))
+    """.format(rank_y=_get_annual_rank(year),
+               rank_y_plus_1=_get_annual_rank(year+1))
 
     stock_table = wrds.raw_sql(stock_query)
 
@@ -82,55 +91,61 @@ def download(wrds, year):
     SELECT begdat, permno, hshrcd FROM crsp_a_stock.dsfhdr
     WHERE begdat <= '{rank_y_plus_1}'
     ORDER BY begdat
-    """.format(rank_y = get_annual_rank(year),
-        rank_y_plus_1 =  get_annual_rank(year+1))
+    """.format(rank_y=_get_annual_rank(year),
+               rank_y_plus_1=_get_annual_rank(year+1))
 
     metadata_table = wrds.raw_sql(metadata_query)
-    
 
     return metadata_table, stock_table
 
 
-#-------------------Index-----------------------
-def filter_by_shrcd(stock, metadata):
-    common_stocks = metadata[(metadata.hshrcd == 10.0) 
-                    | (metadata.hshrcd == 11.0)].permno
-    return stock[stock.permno.isin(common_stocks)]        
+# -------------------Index-----------------------
+def _filter_by_shrcd(stock, metadata):
+    common_stocks = metadata[(metadata.hshrcd == 10.0)
+                             | (metadata.hshrcd == 11.0)].permno
+    return stock[stock.permno.isin(common_stocks)]
 
-def filter_by_price(stock):
+
+def _filter_by_price(stock):
     return stock[(stock["prc"].abs()/stock["cfacpr"] > 1.0)]
 
-def filter_by_cap(stock):
-    return stock[stock["mkt_cap"]>30e6]
-    
-def filter_by_minimum_requirements(stock):
-    return filter_by_cap(filter_by_price(stock))
 
-def filter_by_ranking(stock, index):
+def _filter_by_cap(stock):
+    return stock[stock["mkt_cap"] > 30e6]
+
+
+def _filter_by_minimum_requirements(stock):
+    return _filter_by_cap(_filter_by_price(stock))
+
+
+def _filter_by_ranking(stock, index):
     if index == "3000":
         filter_query = 'mkt_cap <= 3000'
     elif index == "2000":
         filter_query = '1000 < mkt_cap <= 3000'
-    else: 
+    else:
         filter_query = 'mkt_cap <= 1000'
 
     ranked_permco = stock.groupby("permco")["mkt_cap"].sum().rank(
-                    ascending=False).reset_index().query(filter_query).permco
+        ascending=False).reset_index().query(filter_query).permco
 
     return stock[stock.permco.isin(ranked_permco)]
 
-def get_mkt_cap_by_permco(stock):
+
+def _get_mkt_cap_by_permco(stock):
     return stock.groupby("permco",
-                    as_index=False)["mkt_cap"].sum()
-    
-def append_weights(stock):
-    mkt_cap_by_permco = get_mkt_cap_by_permco(stock)
+                         as_index=False)["mkt_cap"].sum()
+
+
+def _append_weights(stock):
+    mkt_cap_by_permco = _get_mkt_cap_by_permco(stock)
     total_mkt_cap = mkt_cap_by_permco.mkt_cap.sum()
     mkt_cap_by_permco["weights"] = (mkt_cap_by_permco.mkt_cap/total_mkt_cap)
-    return stock.merge(mkt_cap_by_permco.drop("mkt_cap",axis=1),
-                on = "permco", how = "left")
+    return stock.merge(mkt_cap_by_permco.drop("mkt_cap", axis=1),
+                       on="permco", how="left")
 
-def get_quarter_dates(quarter,calendar):
+
+def _get_quarter_dates(quarter, calendar):
     if quarter == 3:
         return calendar["Annual Rank Day"], calendar["Q3 Rank Day"]
     elif quarter == 4:
@@ -138,85 +153,97 @@ def get_quarter_dates(quarter,calendar):
     else:
         return calendar["Q4 Rank Day"], calendar["Q1 Rank Day"]
 
-def get_new_IPOs(metadata, calendar, init= None, end = None):
+
+def _get_new_IPOs(metadata, calendar, init=None, end=None):
     return metadata[(metadata.begdat > init) & (metadata.begdat <= end)].permno
 
-def preprocessing_IPOs(metadata, stock, calendar, init = None, end = None):
 
-    IPOs_ids = get_new_IPOs(metadata, calendar, init = init,
-                 end = end)
-    IPOs_stock = stock[(stock.permno.isin(IPOs_ids)) & 
-                (stock.date == end)]
-    filtered_ipos = filter_by_minimum_requirements(IPOs_stock)
+def _preprocessing_IPOs(metadata, stock, calendar, init=None, end=None):
+
+    IPOs_ids = _get_new_IPOs(metadata, calendar, init=init,
+                             end=end)
+    IPOs_stock = stock[(stock.permno.isin(IPOs_ids)) &
+                       (stock.date == end)]
+    filtered_ipos = _filter_by_minimum_requirements(IPOs_stock)
     return filtered_ipos
 
-def filter_by_capitalization(filtered_ipos, index_stocks, index):
-    annual_mkt_cap_by_permco = get_mkt_cap_by_permco(index_stocks[
+
+def _filter_by_capitalization(filtered_ipos, index_stocks, index):
+    annual_mkt_cap_by_permco = _get_mkt_cap_by_permco(index_stocks[
         index_stocks["date"] == index_stocks.date.min()])
     min_cap = annual_mkt_cap_by_permco.mkt_cap.min()
-    if index == "1000" or index == "3000":  
-        #Russell 1000 and 3000 
+    if index == "1000" or index == "3000":
+        # Russell 1000 and 3000
         return filtered_ipos[filtered_ipos.mkt_cap >= min_cap]
     else:
-        #Russell 2000
-        max_cap = annual_mkt_cap_by_permco.mkt_cap.max() 
+        # Russell 2000
+        max_cap = annual_mkt_cap_by_permco.mkt_cap.max()
         return filtered_ipos[(filtered_ipos.mkt_cap >= min_cap) &
-                            (filtered_ipos.mkt_cap <= max_cap)]
+                             (filtered_ipos.mkt_cap <= max_cap)]
 
-def filter_by_companies_in_index(filtered_ipos, index_stocks):
+
+def _filter_by_companies_in_index(filtered_ipos, index_stocks):
     companies_in_index = index_stocks[
         index_stocks["date"] == index_stocks.date.min()].permco.unique()
     return filtered_ipos[~filtered_ipos.permco.isin(companies_in_index)]
 
-def build_annual_weights(calendar, stock, index, metadata):
-    filtered_stocks = filter_by_minimum_requirements(
-                    stock[stock.date == calendar["Annual Rank Day"]])
-    filtered_stocks = filter_by_shrcd(filtered_stocks, metadata)
-    filtered_stocks = filter_by_ranking(filtered_stocks, index)
-    return append_weights(filtered_stocks)
 
-def build_quarterly_weights(index_stocks, metadata, stock, 
-                            calendar, index, quarter):
-    init, end = get_quarter_dates(quarter,calendar)
-    filtered_ipos = preprocessing_IPOs(metadata, stock, calendar,
-                    init = init, end = end)
-    filtered_ipos = filter_by_shrcd(filtered_ipos, metadata)           
-    filtered_ipos = filter_by_capitalization(filtered_ipos,
-                    index_stocks, index)
-    filtered_ipos = filter_by_companies_in_index(filtered_ipos,
-                    index_stocks)
+def _build_annual_weights(calendar, stock, index, metadata):
+    filtered_stocks = _filter_by_minimum_requirements(
+        stock[stock.date == calendar["Annual Rank Day"]])
+    filtered_stocks = _filter_by_shrcd(filtered_stocks, metadata)
+    filtered_stocks = _filter_by_ranking(filtered_stocks, index)
+    return _append_weights(filtered_stocks)
+
+
+def _build_quarterly_weights(index_stocks, metadata, stock,
+                             calendar, index, quarter):
+    init, end = _get_quarter_dates(quarter, calendar)
+    filtered_ipos = _preprocessing_IPOs(metadata, stock, calendar,
+                                        init=init, end=end)
+    filtered_ipos = _filter_by_shrcd(filtered_ipos, metadata)
+    filtered_ipos = _filter_by_capitalization(filtered_ipos,
+                                              index_stocks, index)
+    filtered_ipos = _filter_by_companies_in_index(filtered_ipos,
+                                                  index_stocks)
     new_index_permnos = filtered_ipos.permno.append(index_stocks.permno,
-                        ignore_index = True)
-    new_index_composition  = stock[(stock.date == end) &
-                     (stock.permno.isin(new_index_permnos))]
+                                                    ignore_index=True)
+    new_index_composition = stock[(stock.date == end) &
+                                  (stock.permno.isin(new_index_permnos))]
 
-    return  pd.concat([index_stocks,append_weights(new_index_composition)])
+    return pd.concat([index_stocks, _append_weights(new_index_composition)])
 
-#----------------- Formatting -----------------------
-def beautify_table(table, calendar, year):   
-    table.date =  table.date.replace(
-                    to_replace = match_rank_w_rebalance(calendar,year))
+# ----------------- Formatting -----------------------
+
+
+def _beautify_table(table, calendar, year):
+    table.date = table.date.replace(
+        to_replace=_match_rank_w_rebalance(calendar, year))
     weights_table = pd.pivot_table(table,  values='weights', index=['date'],
-                 columns=['permno','cusip','permco'])
+                                   columns=['permno', 'cusip', 'permco'])
     return weights_table
 
-def reindex_with_bday(weights_table,trading_days, calendar, year):
-    last_bday = get_calendar(year+1)["Annual Rebalance Day"] - BDay(1)
+
+def _reindex_with_bday(weights_table, trading_days, calendar, year):
+    last_bday = _get_calendar(year+1)["Annual Rebalance Day"] - BDay(1)
     trading_days = pd.to_datetime(trading_days)
-    index_from_rebalance = trading_days[trading_days.date >=calendar["Annual Rebalance Day"]]
+    index_from_rebalance = trading_days[trading_days.date >=
+                                        calendar["Annual Rebalance Day"]]
     new_index = index_from_rebalance.append(
-                pd.bdate_range(trading_days[-1],last_bday))
+        pd.bdate_range(trading_days[-1], last_bday))
     return weights_table.reindex(new_index).fillna(method="ffill").fillna(0)
 
-def build(stock, year, index, metadata):
-    calendar = get_calendar(year)
+
+def _build(stock, year, index, metadata):
+
+    calendar = _get_calendar(year)
     stock = stock.assign(mkt_cap=stock.prc.abs()*stock.shrout*1000)
-    index_stocks = build_annual_weights(calendar,
-                    stock,index,metadata)
+    index_stocks = _build_annual_weights(calendar,
+                                         stock, index, metadata)
     if year > 2003:
-        for quarter in [3,4,1]:
-            index_stocks =  build_quarterly_weights(
-                                    index_stocks, metadata, stock, calendar,
-                                    index, quarter)
-    weights_table = beautify_table(index_stocks, calendar, year)    
-    return reindex_with_bday(weights_table,stock.date.unique(),calendar, year)
+        for quarter in [3, 4, 1]:
+            index_stocks = _build_quarterly_weights(
+                index_stocks, metadata, stock, calendar,
+                index, quarter)
+    weights_table = _beautify_table(index_stocks, calendar, year)
+    return _reindex_with_bday(weights_table, stock.date.unique(), calendar, year)
--- pyndex/core/index.py
+++ pyndex/core/index.py
@@ -3,28 +3,53 @@ import pyndex.core.algorithm as alg
 import wrds
 import pandas as pd
 
+
 class Index:
 
     @staticmethod
-    @dec.typeassert(wrds = wrds.sql.Connection,
-                    year = int, index = str)
-    @dec.supported(index = ["1000","2000","3000"])
-    def from_wrds(wrds, year, index, verbose = False):
-        if verbose :
+    @dec.typeassert(wrds=wrds.sql.Connection,
+                    year=int, index=str)
+    @dec.supported(index=["1000", "2000", "3000"])
+    def from_wrds(wrds, year, index, verbose=False):
+        """Generate Russell Indexes from WRDS database.
+
+        Args:
+            wrds (wrds.sql.Connection): Connection to the WRDS database.
+            year (int): Year of the Index to be reconstructed.
+            index (str): Index to reconstructed. Supported Russell 1000, 2000 and 3000.
+            verbose (bool, optional): Verbose output. Defaults to False.
+
+        Returns:
+            pandas.core.DataFrame: DataFrame MultiIndex 
+
+        Examples:
+        Construct an Russell 3000 Index from year 2010.
+        >>> db = wrds.Connection()
+        >>> index = pyndex.Index.from_wrds(db, 2010, "3000")
+
+        """
+        if verbose:
             print("Downloading Data ...")
-        metadata, stock = alg.download(wrds,year)
-        if verbose :
-            print("Building Index ...")
-        weights_table = alg.build(stock, year, index,
-                        metadata)
+        metadata, stock = alg._download(wrds, year)
+        if verbose:
+            print(f"Building Russell {index} year {year} ...")
+        weights_table = alg._build(stock, year, index,
+                                   metadata)
 
         return weights_table
 
     @staticmethod
-    @dec.typeassert(year = int)
+    @dec.typeassert(year=int)
     def get_calendar(year):
-        calendar = alg.get_calendar(year)
+        """Generate Russell reconstruction calendar by year.
+
+        Args:
+            year (int): Year corresponding to calendar.
+
+        Returns:
+            pandas.core.DataFrame: DataFrame containing reconstruction calendar by event.
+        """
+        calendar = alg._get_calendar(year)
         calendar_table = pd.DataFrame(
-                        calendar.items(),columns=["event","date"])
+            calendar.items(), columns=["event", "date"])
         return calendar_table
-                
