911f53fbc7fd1dff673e656192e0c933429f4467
alemicheli <alessandromicheli01@gmail.com>
2020-06-12 11:00:08 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
-    name="pyndex",  # Replace with your own username
+    name="pyndex",  
     version="0.1",
     author="Alessandro Micheli",
     author_email="am1118@imperial.ac.uk",
     description="Pyndex: A python package for Russell index reconstruction.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/alemicheli/pyndex",
     packages=find_packages(),
     classifiers=[
