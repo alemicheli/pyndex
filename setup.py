c35fd81bf57ed4045ef61c3950e17608ab1e699f
alemicheli <alessandromicheli01@gmail.com>
2020-06-12 11:06:33 +0100

    Corrected type in license

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
