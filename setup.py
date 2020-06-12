4b4a6de28cab124d4b7ff16bafa4ed8a340967fc
alemicheli <alessandromicheli01@gmail.com>
2020-06-12 14:05:35 +0100

    changed name to pyndex-fin

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="pyndex",  
+    name="pyndex-fin",  
     version="0.1",
     author="Alessandro Micheli",
     author_email="am1118@imperial.ac.uk",
